4a89ab5deccbac3619b565ef0081d7babeb6d9b5
Thomas Groechel <groechel@usc.edu>
2019-12-19 13:01:34 -0500

    Remade as math instruction

---
+++ Assets/Scripts/MathInstruction.cs
@@ -0,0 +1,23 @@
+﻿namespace MoveToCode {
+    public abstract class MathInstruction : Instruction {
+        protected int leftInt, rightInt, numArguments = 2;
+
+        public MathInstruction() {
+            ResizeArgumentList(numArguments);
+        }
+
+        public MathInstruction(IntDataType intLeft, IntDataType intRight) {
+            SetArgumentAt(intLeft);
+            SetArgumentAt(intRight);
+        }
+
+        public override void EvaluateArgumentList() {
+            leftInt = argumentList[0].EvaluateArgument().GetValue();
+            rightInt = argumentList[1].EvaluateArgument().GetValue();
+        }
+
+        public override int GetNumArguments() {
+            return numArguments;
+        }
+    }
+}
--- Assets/Scripts/SubtractionInstruction.cs
+++ Assets/Scripts/SubtractionInstruction.cs
@@ -1,26 +1,5 @@
 ﻿namespace MoveToCode {
-    public class SubtractionInstruction : Instruction {
-
-        int leftInt, rightInt, numArguments = 2;
-
-        public SubtractionInstruction() {
-            ResizeArgumentList(numArguments);
-        }
-
-        public SubtractionInstruction(IntDataType intLeft, IntDataType intRight) {
-            SetArgumentAt(intLeft);
-            SetArgumentAt(intRight);
-        }
-
-        public override void EvaluateArgumentList() {
-            leftInt = argumentList[0].EvaluateArgument().GetValue();
-            rightInt = argumentList[1].EvaluateArgument().GetValue();
-        }
-
-        public override int GetNumArguments() {
-            return numArguments;
-        }
-
+    public class SubtractionInstruction : MathInstruction {
         public override InstructionReturnValue RunInstruction() {
             EvaluateArgumentList();
             return new InstructionReturnValue(new IntDataType(leftInt - rightInt), GetNextInstruction());

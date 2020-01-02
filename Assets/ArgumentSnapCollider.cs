4d2e09fef91d757a352861bcab4c0bff4114517c
Thomas Groechel <groechel@usc.edu>
2020-01-02 09:59:00 -0500

    Added argument collider, need check before doing action

---
+++ Assets/ArgumentSnapCollider.cs
@@ -0,0 +1,8 @@
+﻿namespace MoveToCode {
+    public class ArgumentSnapCollider : SnapCollider {
+        public int myInstructionPosition = 0;
+        public override void DoSnapAction(CodeBlock myCodeBlock, CodeBlock collidedCodeBlock) {
+            myCodeBlock.SetArgumentBlockAt(collidedCodeBlock, myInstructionPosition);
+        }
+    }
+}

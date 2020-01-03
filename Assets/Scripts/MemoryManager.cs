44aa28e27ad494a864ffd522bb0f03b6cdf64ee4
Thomas Groechel <groechel@usc.edu>
2020-01-03 12:20:29 -0500

    Starting to create variables

---
+++ Assets/Scripts/MemoryManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MoveToCode {
+    public class MemoryManager : Singleton<MemoryManager> {
+        Dictionary<Variable, IDataType> variables;
+        protected CodeBlock myCodeBlock;
+
+        private void Awake() {
+            variables = new Dictionary<Variable, IDataType>();
+        }
+
+        public IDataType GetVariableValue(Variable vIn) {
+            return variables[vIn];
+        }
+
+        public void AddVariable(Variable vIn, IDataType dIn) {
+            variables.Add(vIn, dIn);
+        }
+
+        public CodeBlock GetCodeBlock() {
+            return myCodeBlock;
+        }
+
+        public void SetCodeBlock(CodeBlock codeBlock) {
+            myCodeBlock = codeBlock;
+        }
+    }
+}
---
+++ Assets/Scripts/Variable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MoveToCode {
+    public class Variable : IArgument {
+        public IDataType EvaluateArgument() {
+            return MemoryManager.instance.GetVariableValue(this);
+        }
+
+        public CodeBlock GetCodeBlock() {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetCodeBlock(CodeBlock codeBlock) {
+            throw new System.NotImplementedException();
+        }
+    }
+}

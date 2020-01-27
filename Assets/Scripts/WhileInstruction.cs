31ba9311bedc2723430bd2e688256d1e843b04e5
Thomas Groechel <groechel@usc.edu>
2020-01-27 07:58:33 -0800

    Initial while loop working, needs refactor with if

---
+++ Assets/Scripts/WhileInstruction.cs
@@ -0,0 +1,43 @@
+﻿namespace MoveToCode {
+    public class WhileInstruction : ControlFlowInstruction {
+
+        bool conditionIsTrue;
+        bool exitInstructionAddedToStack;
+
+        public WhileInstruction() {
+            ResizeArgumentList(GetNumArguments());
+            exitInstructionAddedToStack = false;
+        }
+
+        public override void EvaluateArgumentList() {
+            conditionIsTrue = (argumentList[0] as ConditionalInstruction)?.RunInstruction().GetReturnDataVal().GetValue();
+        }
+
+        public override InstructionReturnValue RunInstruction() {
+            if (!exitInstructionAddedToStack) {
+                Interpreter.instance.AddToInstructionStack(GetExitInstruction());
+                exitInstructionAddedToStack = true;
+            }
+            EvaluateArgumentList();
+            if (conditionIsTrue) {
+                // put me on top of stack when while loop ends
+                Interpreter.instance.AddToInstructionStack(this);
+                return new InstructionReturnValue(null, GetNextInstruction());
+            }
+            return null; // done with loop
+        }
+
+
+        public override void SetUpArgumentCompatability() {
+            compatibileArgumentTypes.Add(typeof(ConditionalInstruction));
+        }
+
+        public override int GetNumArguments() {
+            return 1;
+        }
+
+        public override string ToString() {
+            return string.Join("", "While: ", argumentList[0]?.ToString());
+        }
+    }
+}
---
+++ Assets/WhileCodeBlock.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace MoveToCode {
+    public class WhileCodeBlock : ControlFlowCodeBlock {
+        public override int GetBlockVerticalSize() {
+            return 3;
+        }
+
+        public override Vector3 GetSnapToParentPosition() {
+            return new Vector3(0.25f, 0, 0);
+        }
+
+        protected override void SetMyBlockInternalArg() {
+            myBlockInternalArg = new WhileInstruction();
+        }
+    }
+}

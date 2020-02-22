5948396860434deab2aee5981df29d6e5ced5fd5
Thomas Groechel <groechel@usc.edu>
2020-02-22 12:03:08 -0800

    Physical affective emotions added

--- Assets/Scripts/KeyboardTestingScript.cs
+++ Assets/Scripts/KeyboardTestingScript.cs
@@ -6,36 +6,18 @@ using UnityEngine;
 
 public class KeyboardTestingScript : MonoBehaviour {
 
-    IntDataType i, i2;
-    PrintInstruction pi;
-    PrintInstruction p2;
-    SubtractionInstruction st;
-
-    public CodeBlock pb1, pb2, pb3, dt1, dt2, dt3, subCB;
-
-    void Start() {
-        // Need to redo codeblock structure/adding args
-        /* i = new IntDataType(4);
-         i2 = new IntDataType(9);
-         pi = new PrintInstruction(i);
-         p2 = new PrintInstruction(i);
-         st = new SubtractionInstruction(i, i2);
-         pi.SetNextInstruction(p2);
-         pi.SetArgumentAt(st, 0);*/
-        //Interpreter.instance.AddToInstructionStack(pb1.GetMyInstruction());
-    }
-
-
-
     void Update() {
         if (Input.GetKeyDown(KeyCode.Alpha0)) {
             Interpreter.instance.RunNextInstruction();
         }
-        if (Input.GetKeyDown(KeyCode.Alpha0)) {
-            FindObjectOfType<PoseStampedPublisher>().PublishPosition();
-        }
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
 
-        // use full name usually
+            FindObjectOfType<KuriEmoteStringPublisher>().PubRandomPositive();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
+
+            FindObjectOfType<KuriEmoteStringPublisher>().PubRandomNegative();
+        }
 
     }
 }

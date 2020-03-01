39f6a4ca0ec7a3f8a17424e8dcf826c94d2f025e
Thomas Groechel <groechel@usc.edu>
2020-03-01 10:41:31 -0800

    Interval policy done

--- Assets/Scripts/InteractionManager.cs
+++ Assets/Scripts/InteractionManager.cs
@@ -4,30 +4,43 @@ using UnityEngine;
 namespace MoveToCode {
     public class InteractionManager : Singleton<InteractionManager> {
         public float fullInteractionTimeMinutes, warmUpTimeMinutes;
-        public float[] kcCond = { 0, 0 };
+        public float startKC = 1f;
+        public float numIntervals = 10;
+        float reallyHighKC = 700f;
+        public bool startLow;
         bool kcState;
 
         private void Start() {
-            StartCoroutine(WaitToSwap());
+            StartCoroutine(PolicySwapCoroutine());
         }
 
         public float MinToSeconds(float f) {
             return f * 60f;
         }
 
-        IEnumerator WaitToSwap() {
-            KuriManager.instance.SetKC(ChooseKCR());
-            yield return new WaitForSeconds((MinToSeconds(fullInteractionTimeMinutes) - MinToSeconds(warmUpTimeMinutes)) / 2.0f);
-            KuriManager.instance.SetKC(ChooseKCR());
-            yield return new WaitForSeconds((MinToSeconds(fullInteractionTimeMinutes) - MinToSeconds(warmUpTimeMinutes)) / 2.0f);
+        IEnumerator PolicySwapCoroutine() {
+            KuriManager.instance.SetKC(reallyHighKC);
+            yield return new WaitForSeconds(MinToSeconds(warmUpTimeMinutes));
+            float timeLeft = MinToSeconds(fullInteractionTimeMinutes) - MinToSeconds(warmUpTimeMinutes);
+            float intervalTime = timeLeft / numIntervals;
+            for (int i = 0; i < numIntervals; ++i) {
+                KuriManager.instance.SetKC(ChooseKCRAtInterval(i));
+                yield return new WaitForSeconds(intervalTime);
+            }
             Application.Quit();
         }
 
-        // TODO: might make this into my choice before hand
-        public float ChooseKCR() {
-            int choice = Random.Range(0, kcCond.Length);
-            float result = kcCond[choice];
-            kcCond[choice] = kcCond[(choice + 1) % kcCond.Length];
+        public float ChooseKCRAtInterval(int atInterval) {
+            float result = startKC;
+            float addition = atInterval * (2.0f / (numIntervals - 1));
+            if (startLow) {
+                result = -startKC;
+                result += addition;
+            }
+            else {
+                result -= addition;
+            }
+            Debug.Log(result);
             return result;
         }
     }

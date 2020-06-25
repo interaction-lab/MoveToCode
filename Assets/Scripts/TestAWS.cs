7c7cc0314ed9a6ca3774556e7c03394abc420a49
quickTwix898 <54284925+quickTwix898@users.noreply.github.com>
2020-06-25 09:33:26 -0700

    Initial commit for branch. Some new AWS SDK packages and testing

---
+++ Assets/Scripts/TestAWS.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Amazon.Polly;
+using Amazon.Polly.Model;
+using System.IO;
+using System;
+using System.Text;
+
+public class TestAWS : MonoBehaviour
+{
+    private AmazonPollyClient apc;
+    private SynthesizeSpeechResponse sres;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        try {
+            apc = new AmazonPollyClient("AKIAUTHIP3ELD5TBOOHO", "TZdPzUStylOlbqSI10YidqMRSta+bInm2Z69Wx7t", Amazon.RegionEndpoint.USWest1);
+        } catch (Exception e) {
+            Debug.Log("Error Generating Polly client with credentials: " + e.Message);
+        }
+
+        SynthesizeSpeechRequest sreq = new SynthesizeSpeechRequest() {
+            Text = "Good Job!",
+            VoiceId = VoiceId.Joanna,
+            OutputFormat = OutputFormat.Mp3
+        };
+        //TODO simplify try catches
+        try {
+            sres = apc.SynthesizeSpeech(sreq);
+        } catch(Exception e) {
+            Debug.Log("Error getting speech response from client: " + e.Message);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        foreach (char c in sreq.Text) {
+            if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_') {
+                sb.Append(c);
+            }
+        }
+
+        try {
+            using (var fileStream = File.Create(@"Assets\Resources\Audio\Speech\" + sb.ToString() + ".mp3")) {
+                sres.AudioStream.CopyTo(fileStream);
+                fileStream.Flush();
+                fileStream.Close();
+                Debug.Log("File created succesfully");
+            }
+        } catch(Exception e) {
+            Debug.Log("Error writing speech to file: " + e.Message);
+        }
+
+        AudioClip audioClip = Resources.Load<AudioClip>("Audio/Speech/GoodJob.mp3");
+        audioSource.clip = audioClip;
+        audioSource.Play();
+
+    }
+
+}

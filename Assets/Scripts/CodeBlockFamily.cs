613b99dccd1842857165acb21a99648d5df338b2
Chloe Kuo <cmkuo@usc.edu>
2020-07-20 01:12:15 -0700

    Update

--- Assets/Scripts/CodeBlockFamily.cs
+++ Assets/Scripts/CodeBlockFamily.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.MixedReality.Toolkit.UI;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace MoveToCode {
     public class CodeBlockFamily : MonoBehaviour {
 
-        public GameObject[] blocksInFamily;
+        public List<GameObject> blocksInFamily;
         MeshRenderer shelfMeshRenderer;
 
         private void Awake() {
@@ -14,6 +16,7 @@ namespace MoveToCode {
         }
 
         private void Start() {
+            AddBlocksToFamily();
             InstantiateBlocksInFamily();
         }
 
@@ -25,32 +28,64 @@ namespace MoveToCode {
             gameObject.SetActive(false);
         }
 
-        public void InstantiateBlocksInFamily() {
-            for (int i = 0; i < blocksInFamily.Length; i++) {
-                GameObject go = Instantiate(blocksInFamily[i]);
-                //Vector3 currBlockSize = GetBlockSize(go);
-                go.transform.position = new Vector3(shelfMeshRenderer.bounds.center.x, shelfMeshRenderer.bounds.extents.y - 0.25f - i * GetBlockHeight(go)/2.5f, transform.position.z);
-                
-                //set codeblocks as blocks in menu
-                go.AddComponent<CloneOnDrag>().SetCodeBlockType(blocksInFamily[i]);
-                go.GetComponent<CodeBlock>().SetIsMenuBlock(true);
-                go.transform.SnapToParent(transform);
-                go.transform.localScale = Vector3.one;
+        private void AddBlocksToFamily() {
+            if (blocksInFamily[0].GetComponent<ConditionalCodeBlock>() != null) {
+                for (int i = 1; i < System.Enum.GetNames(typeof(ConditionalCodeBlock.OPERATION)).Length; i++) {
+                    //blocksInFamily.Add(blocksInFamily[0].GetComponent<);
+                    SetConditionalOperation(blocksInFamily[0], i);
+                    GameObject go = Instantiate(blocksInFamily[0]);
+                    SetBlockPosition(go, 0, i);
+                }
+            } else if (blocksInFamily[0].GetComponent<MathOperationCodeBlock>() != null) {
+                for (int i = 1; i < System.Enum.GetNames(typeof(MathOperationCodeBlock.OPERATION)).Length; i++) {
+                    SetMathOperation(blocksInFamily[0], i);
+                    GameObject go = Instantiate(blocksInFamily[0]);
+                    SetBlockPosition(go, 0, i);
+                }
             }
         }
 
-        /*private Vector3 GetBlockSize(GameObject goIn) {
-            Vector3 size = Vector3.zero;
-            goIn.transform.GetChild(0);
-            int debugCount = 0;
-            MeshRenderer[] allObjectRenderers = goIn.GetComponentsInChildren<MeshRenderer>();
-            foreach (MeshRenderer mRenderer in allObjectRenderers) {
-                size += mRenderer.bounds.size;
-                debugCount++;
+        public void InstantiateBlocksInFamily() {
+            if (blocksInFamily[0].GetComponent<ConditionalCodeBlock>() != null) {
+                for (int i = 0; i < System.Enum.GetNames(typeof(ConditionalCodeBlock.OPERATION)).Length; i++) {
+                    SetConditionalOperation(blocksInFamily[0], i);
+                    GameObject go = Instantiate(blocksInFamily[0]);
+                    SetBlockPosition(go, 0, i);
+                }
+            } else if (blocksInFamily[0].GetComponent<MathOperationCodeBlock>() != null) {
+                for (int i = 0; i < System.Enum.GetNames(typeof(MathOperationCodeBlock.OPERATION)).Length; i++) {
+                    SetMathOperation(blocksInFamily[0], i);
+                    GameObject go = Instantiate(blocksInFamily[0]);
+                    SetBlockPosition(go, 0, i);
+                }
+            } else {
+                for (int i = 0; i < blocksInFamily.Count; i++) {
+                    GameObject go = Instantiate(blocksInFamily[i]);
+                    SetBlockPosition(go, i, i);
+                }
             }
-            Debug.Log(debugCount);
-            return size;
-        }*/
+        }
+
+        private void SetBlockPosition(GameObject block, int index, int blockNum) {
+            block.transform.position = new Vector3(
+                    shelfMeshRenderer.bounds.center.x,
+                    shelfMeshRenderer.bounds.extents.y - 0.25f - blockNum * shelfMeshRenderer.bounds.size.y / (blocksInFamily.Count + 1),
+                    transform.position.z);
+
+            //set codeblocks as blocks in menu
+            block.AddComponent<CloneOnDrag>().SetCodeBlockType(blocksInFamily[index]);
+            block.GetComponent<CodeBlock>().SetIsMenuBlock(true);
+            block.transform.SnapToParent(transform);
+            block.transform.localScale = Vector3.one;
+        }
+
+        private void SetMathOperation(GameObject block, int index) {
+            block.GetComponent<MathOperationCodeBlock>().SetOperation((MathOperationCodeBlock.OPERATION)index);
+        }
+
+        private void SetConditionalOperation(GameObject block, int index) {
+            block.GetComponent<ConditionalCodeBlock>().SetOperation((ConditionalCodeBlock.OPERATION)index);
+        }
 
         private float GetBlockHeight(GameObject goIn) {
             float height = goIn.GetComponentInChildren<CodeBlockObjectMesh>().GetBlockVerticalSize();

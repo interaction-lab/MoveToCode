71b00871bf25a997532cbbe5992da1b3ef0c1610
Karen Ly <karenly@usc.edu>
2021-02-10 12:07:44 -0800

    Create Firebase initialization script

---
+++ Assets/Scripts/FirebaseInit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using Firebase;
+using Firebase.Storage;
+
+
+public class FirebaseInit : MonoBehaviour
+{
+    public UnityEvent OnFirebaseInitialized = new UnityEvent();
+    private async void Start()
+    {
+        var dependencyStatus = await FirebaseApp.CheckAndFixDependenciesAsync();
+        if (dependencyStatus == DependencyStatus.Available)
+        {
+            OnFirebaseInitialized.Invoke();
+        }
+    }
+}

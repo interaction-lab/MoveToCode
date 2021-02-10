3286bdc758c204a8d1b85ef2c83e0b34adc756a1
Karen Ly <karenly@usc.edu>
2021-02-10 12:14:25 -0800

    Create upload file to Firebase storage script

---
+++ Assets/Scripts/UploadFile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using Firebase.Storage;
+using UnityEngine;
+
+public class UploadFile : MonoBehaviour
+{
+
+    public void StartUpload(TextAsset csvFile)
+    {
+        StartCoroutine(UploadCoroutine(csvFile));
+    }
+
+    private IEnumerator UploadCoroutine(TextAsset csvFile)
+    {
+        //  throw new System.NotImplementedException();
+        var storage = FirebaseStorage.DefaultInstance;
+        var csvReference = storage.GetReference($"/csvfiles/{Guid.NewGuid()}.csv");
+        var bytes = csvFile.bytes;
+        var uploadTask = csvReference.PutBytesAsync(bytes);
+        yield return new WaitUntil(() => uploadTask.IsCompleted);
+
+        if (uploadTask.Exception != null)
+        {
+            Debug.LogError($"Failed to upload because {uploadTask.Exception}");
+            yield break;
+        }
+
+        // checks if uploaded correctly
+        var getUrlTask = csvReference.GetDownloadUrlAsync();
+        yield return new WaitUntil(() => getUrlTask.IsCompleted);
+
+        if (getUrlTask.Exception != null)
+        {
+            Debug.LogError($"Failed to get a download url with {getUrlTask.Exception}");
+            yield break;
+
+        }
+
+        Debug.Log($"Download from {getUrlTask.Result}");
+
+    }
+}
+

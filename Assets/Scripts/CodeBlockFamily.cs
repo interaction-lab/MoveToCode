1c90ffba0c3baa4b3ee47cae81ba0f58bd49f36a
Ipek Goktan <ipekg@usc.edu>
2021-02-11 19:19:57 -0800

    fixing code block family positioning

--- Assets/Scripts/CodeBlockFamily.cs
+++ Assets/Scripts/CodeBlockFamily.cs
@@ -41,6 +41,7 @@ namespace MoveToCode {
         }
 
         private void InstantiateBlocksInFamily() {
+            
             for (int i = 0; i < blocksInFamily.Count; i++) {
                 SetMathOperation(blocksInFamily[0], i);
                 SetConditionalOperation(blocksInFamily[0], i);
@@ -50,16 +51,17 @@ namespace MoveToCode {
         }
 
         private void SetBlockPosition(GameObject block, int index) {
-            block.transform.position = new Vector3(
+            block.transform.SnapToParent(transform);
+            block.transform.localScale = Vector3.one;
+            block.transform.localPosition = new Vector3(
                     shelfMeshRenderer.bounds.center.x,
                     shelfMeshRenderer.bounds.extents.y - 0.2f - index * shelfMeshRenderer.bounds.size.y / (blocksInFamily.Count + 0.25f),
-                    transform.position.z);
+                    transform.localPosition.z + 1);
 
             //set codeblocks as blocks in menu
             block.GetComponent<CloneOnDrag>().SetCodeBlockType(blocksInFamily[index]);
             block.GetComponent<CodeBlock>().SetIsMenuBlock(true);
-            block.transform.SnapToParent(transform);
-            block.transform.localScale = Vector3.one;
+            
         }
 
         private void SetMathOperation(GameObject block, int index) {

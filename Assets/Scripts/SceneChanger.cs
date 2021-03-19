2b71338d9cd562d8e93f61112f33c8ba3e558d0a
Ipek Goktan <ipekg@usc.edu>
2021-03-19 14:39:54 -0700

    adjusting scales, naming buttons

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+ //    public void ChangeScene(string sceneName)
+	// {
+	// 	SceneManager.LoadScene (name);
+	// }
+	// public void Exit()
+	// {
+	// 	Application.Quit ();
+	// }
+}

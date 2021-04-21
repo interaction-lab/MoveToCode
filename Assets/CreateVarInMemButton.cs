2dd4367f2592afee076bbf1a5835a7a843a8ebfe
tgroechel <groechel@usc.edu>
2021-04-21 07:56:25 -0700

    Added rotate button and fixed reference for spawning variables

---
+++ Assets/CreateVarInMemButton.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace MoveToCode {
+    public class CreateVarInMemButton : MonoBehaviour {
+        Interactable interactable;
+
+        private void Awake() {
+            interactable = GetComponent<Interactable>();
+            interactable.OnClick.AddListener(SpawnVariableBlock);
+        }
+
+        private void SpawnVariableBlock() {
+            FreePlayMenuManager.instance.InstantiateVariableBlockCollection();
+        }
+    }
+}
--- Assets/Scripts/MakeTransformLookAtUser.cs
+++ Assets/Scripts/MakeTransformLookAtUser.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 
 namespace MoveToCode {
     public class MakeTransformLookAtUser : MonoBehaviour {
+        public Transform t;
         public AnimationCurve animCurve;
 
+        private void Awake() {
+            if (t == null) {
+                t = transform;
+            }
+        }
         IEnumerator Turn() {
             Quaternion start = transform.rotation, goal = Quaternion.LookRotation(Camera.main.transform.forward);
             float curTime = 0f, totalTime = 0.5f;
 
             while (curTime < totalTime) {
                 float percent = curTime / totalTime;
-                transform.localRotation = Quaternion.Lerp(start, goal, animCurve.Evaluate(percent));
+                t.localRotation = Quaternion.Lerp(start, goal, animCurve.Evaluate(percent));
                 yield return new WaitForSeconds(Time.deltaTime);
                 curTime += Time.deltaTime;
             }

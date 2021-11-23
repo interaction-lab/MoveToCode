311681c922a258fb59cd9d4453378cf0dd7d7a78
Tom Groechel <tgroeche@umich.edu>
2021-11-23 07:41:55 -0800

    Cundle id updated

--- Assets/Scripts/SnapColliderNested.cs
+++ Assets/Scripts/SnapColliderNested.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 namespace MoveToCode {
     public class SnapColliderNested : SnapCollider {
-        public override Vector3 SnapPosition { get; } = new Vector3(0.2f, -1.0f, 0f);
+        public override Vector3 SnapPosition { 
+            get{
+            return new Vector3(0.15f, transform.localPosition.y, 0);
+        } }
 
         public override HashSet<Type> CompatibleArgTypes { get; }
             = new HashSet<Type> { typeof(SnappableStandAloneInstruction) };

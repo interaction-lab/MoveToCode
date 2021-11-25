1e3b6d31308c62e5aab4d623728af41cc72cdaff
Tom Groechel <tgroeche@umich.edu>
2021-11-25 08:16:17 -0800

    Kuri text updated and local ip single skip

--- Assets/UserIPAddr.cs
+++ Assets/UserIPAddr.cs
@@ -6,7 +6,9 @@ namespace MoveToCode {
         public string GetLocalIPAddress() {
             var host = Dns.GetHostEntry(Dns.GetHostName());
             foreach (var ip in host.AddressList) {
-                if (ip.AddressFamily == AddressFamily.InterNetwork || ip.AddressFamily == AddressFamily.InterNetworkV6) {
+                if ((ip.AddressFamily == AddressFamily.InterNetwork 
+                    || ip.AddressFamily == AddressFamily.InterNetworkV6) 
+                    && ip.ToString() != "127.0.0.1") { // Ignore local host
                     return ip.ToString();
                 }
             }

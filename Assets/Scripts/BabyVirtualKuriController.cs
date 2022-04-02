87eb93ae1ae98be581029810a41fd55b807646b4
Tom Groechel <tgroeche@umich.edu>
2022-04-02 12:58:41 -0700

    #95 before maze regions

--- Assets/Scripts/BabyVirtualKuriController.cs
+++ Assets/Scripts/BabyVirtualKuriController.cs
@@ -146,7 +146,7 @@ namespace MoveToCode {
             ResetCurMovementAction();
             StartNextMovement();
         }
-
+        
         private void ResetCurMovementAction() {
             CurMovementAction = "";
         }

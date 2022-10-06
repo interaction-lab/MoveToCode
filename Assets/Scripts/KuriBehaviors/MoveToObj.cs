e42695f2643704aaa832d0415e8a8ff27c3a6592
tgroechel <groechel@usc.edu>
2022-10-06 11:06:26 -0500

    Bezier ok, need to figure out reinit

--- Assets/Scripts/KuriBehaviors/MoveToObj.cs
+++ Assets/Scripts/KuriBehaviors/MoveToObj.cs
@@ -7,11 +7,11 @@ namespace MoveToCode {
     public class MoveToObj : ActionNode {
         TutorKuriTransformManager kuriTransformManager;
         Transform goalObj;
-        float distThreshold = 0.1f, speedinMS = 1f;
+        float distThreshold = 0.1f, speedinMS = 0.5f, bezTimeThreshold = 0.95f;
         float approxLength, totalTime;
         float positionAlongCurve = 0f;
         Bezier bezierCurve;
-        Vector3 objPosWhenBezWasCalculated;
+        Vector3 objPosWhenBezWasCalculated, start, end;
         protected override void OnStart() {
             goalObj = blackboard.objToMoveTo;
             kuriTransformManager = TutorKuriTransformManager.instance;
@@ -20,16 +20,29 @@ namespace MoveToCode {
         }
 
         private void CalcBezCurve() {
+            start = kuriTransformManager.Position;
+            end = goalObj.position;
+            end.y = kuriTransformManager.GroundYCord;
+
+            Vector3 lineVec = end - start;
+
+            // calculate the length of the line
+            float approxLineLength = lineVec.magnitude;
+
+
+            Vector3 tangent = (lineVec);
+            Vector3 normal = Vector3.Cross(tangent, Vector3.up);
+            Vector3 controlPoint = start + tangent * 0.5f + normal * 0.5f;
+
             bezierCurve = new Bezier(
-                Bezier.BezierType.Linear,
-                new Vector3[]{
-                    kuriTransformManager.Position,
-                    goalObj.position});
+                Bezier.BezierType.Quadratic,
+                new Vector3[3] { start, controlPoint, end });
+
             objPosWhenBezWasCalculated = goalObj.position;
             approxLength = bezierCurve.ApproximateTotalLength();
             totalTime = approxLength / speedinMS;
         }
-        
+
         protected override void OnStop() {
         }
 
@@ -39,13 +52,17 @@ namespace MoveToCode {
                 CalcBezCurve();
             }
             // check if they are close
-            if (Vector3.Distance(kuriTransformManager.Position, goalObj.position) < distThreshold) {
+            if (Vector3.Distance(kuriTransformManager.Position, end) < distThreshold) {
                 return State.Success;
             }
 
             // move toward goal using bezier curve
             positionAlongCurve += Time.deltaTime / totalTime;
             kuriTransformManager.Position = bezierCurve.GetBezierPoint(positionAlongCurve);
+
+            if (positionAlongCurve >= bezTimeThreshold) {
+                return State.Success; // return if they are on top of the end
+            }
             return State.Running;
         }
     }

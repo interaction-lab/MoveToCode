8731796dcc0beef9e5e5bf699190ac610e9805dc
Tom Groechel <tgroeche@umich.edu>
2022-10-17 09:51:27 -0500

    Fixed high five

--- Assets/Scripts/KuriBehaviors/MonitorDecorator.cs
+++ Assets/Scripts/KuriBehaviors/MonitorDecorator.cs
@@ -3,6 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace TheKiwiCoder {
-    public abstract class MonitorDecorator : DecoratorNode {
+    public class MonitorDecorator : DecoratorNode {
+        public override Node Clone() {
+            return base.Clone();
+        }
+
+        public override bool Equals(object other) {
+            return base.Equals(other);
+        }
+
+        public override int GetHashCode() {
+            return base.GetHashCode();
+        }
+
+        public override void OnDrawGizmos() {
+            base.OnDrawGizmos();
+        }
+
+        public override string ToString() {
+            return base.ToString();
+        }
+
+        protected override void OnStart() {
+
+        }
+
+        protected override void OnStop() {
+        }
+
+        protected override State OnUpdate() {
+            return child.Update();
+        }
     }
 }
